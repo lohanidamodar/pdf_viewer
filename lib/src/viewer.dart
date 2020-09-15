208151a0bb1d24ae12cd91524d4224b2758bc26a
WianSnyman <w_snyman@hotmail.com>
2020-09-15 21:50:45 +0200

    Update viewer.dart

--- lib/src/viewer.dart
+++ lib/src/viewer.dart
@@ -191,7 +191,7 @@ class _PDFViewerState extends State<PDFViewer> {
       body: Stack(
         children: <Widget>[
           PageView.builder(
-            physics: _swipeEnabled && widget.enableSwipeNavigation
+            physics: _swipeEnabled && widget.enableSwipeNavigation  && !_isLoading
                 ? null
                 : NeverScrollableScrollPhysics(),
             onPageChanged: (page) {
@@ -242,7 +242,7 @@ class _PDFViewerState extends State<PDFViewer> {
                         child: IconButton(
                           icon: Icon(Icons.first_page),
                           tooltip: widget.tooltip.first,
-                          onPressed: _pageNumber == 1
+                          onPressed: _pageNumber == 1 || _isLoading
                               ? null
                               : () {
                                   _pageNumber = 1;
@@ -254,7 +254,7 @@ class _PDFViewerState extends State<PDFViewer> {
                         child: IconButton(
                           icon: Icon(Icons.chevron_left),
                           tooltip: widget.tooltip.previous,
-                          onPressed: _pageNumber == 1
+                          onPressed: _pageNumber == 1 || _isLoading
                               ? null
                               : () {
                                   _pageNumber--;
@@ -271,8 +271,8 @@ class _PDFViewerState extends State<PDFViewer> {
                       Expanded(
                         child: IconButton(
                           icon: Icon(Icons.chevron_right),
-                          tooltip: widget.tooltip.next,
-                          onPressed: _pageNumber == widget.document.count
+                          tooltip: widget.tooltip.next, 
+                          onPressed: _pageNumber == widget.document.count || _isLoading
                               ? null
                               : () {
                                   _pageNumber++;
@@ -287,7 +287,7 @@ class _PDFViewerState extends State<PDFViewer> {
                         child: IconButton(
                           icon: Icon(Icons.last_page),
                           tooltip: widget.tooltip.last,
-                          onPressed: _pageNumber == widget.document.count
+                          onPressed: _pageNumber == widget.document.count || _isLoading
                               ? null
                               : () {
                                   _pageNumber = widget.document.count;
